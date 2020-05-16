52d5722641ddef5affb640445d42cd93086478b0
Sam Marston <sammarston15@gmail.com>
2020-05-16 11:21:26 -0600

    still getting set up

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const app = express();
+const socketio = require('socket.io');
+
+const server = http.createServer(app);
+const io = socketio(server);
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// run when a client connects
+io.on('connection', socket => {
+    console.log('connected to socket!');
+})
 
 const PORT = 3000 || process.env.PORT
 
