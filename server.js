91cf5aa8562dd3efb102d22fb18e942633876088
Sam Marston <sammarston15@gmail.com>
2020-08-23 20:32:22 -0600

    get message to client when user enters & leaves

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const app = express();
 const socketio = require('socket.io');
 
+const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
@@ -14,20 +14,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 io.on('connection', socket => {
 
     // welcome current user
-    socket.emit('message', 'welcome to chat app');
+    socket.emit('message', 'welcome to the chat app'); // socket.emit() sends to just the client joining
 
     // broadcast when a user connects
-    socket.broadcast.emit('message', 'a user has joined the chat!');
+    socket.broadcast.emit('message', 'a user has joined the chat!'); // socket.broadcast.emit() sends to everyone in the room
 
     // runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', 'A user has left the chat'); // io.emit() sends to everyone in the room
     })
 
 })
 
 const PORT = 3000 || process.env.PORT
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`server running on ${PORT}`);
 })
